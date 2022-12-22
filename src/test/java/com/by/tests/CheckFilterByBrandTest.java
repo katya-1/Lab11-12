4f01751779cf772aaf7fe428ad324dd2b074f57f
Katya <katya.bulgak04@gmail.com>
2022-12-22 04:46:58 +0300

    fix code

--- src/test/java/com/by/tests/CheckFilterByBrandTest.java
+++ src/test/java/com/by/tests/CheckFilterByBrandTest.java
@@ -1,28 +1,22 @@
 package com.by.tests;
 
-import com.by.pages.CartPage;
 import com.by.pages.CatalogPage;
-import com.by.pages.ProductPage;
 import org.testng.annotations.Test;
 
 import static com.by.conts.TestConstant.BRAND_NAME;
-import static com.by.conts.TestConstant.CART_EMPTY;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 public class CheckFilterByBrandTest extends CommonConditions {
     @Test
     public void testFilterByBrand() {
-            ProductPage productPage = new ProductPage(driver);
-            CartPage cartPage = new CartPage(driver);
+        CatalogPage catalogPage = new CatalogPage(driver);
 
-            productPage.openPage()
-                    .clickAddToCartButton()
-                    .clickGoToCart();
-            cartPage.openPage()
-                    .clickRemoveButton();
-            String resultEmptyCart = cartPage.getCartEmpty();
+        catalogPage.openPage()
+                .clickBrandButton()
+                .clickChooseBrandButton();
+        String resultBrandProduct = catalogPage.getBrandProduct();
 
-            assertThat(resultEmptyCart, is(CART_EMPTY));
+        assertThat(resultBrandProduct, is(BRAND_NAME));
     }
 }
